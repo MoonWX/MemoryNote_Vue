2faf9cd896cbc40cdc1ab4d90a29e723d29f9e43
Chisato <zoneherobrine@gmail.com>
2024-07-15 22:00:12 +0800

    0.0.2 配置i18n和vueuse

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 
@@ -9,9 +10,12 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const i18n = createI18n({})
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.use(i18n)
 app.use(PrimeVue, {
     theme: {
         preset: Aura
